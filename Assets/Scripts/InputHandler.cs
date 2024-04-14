75ad7cf0341a1e28f9f7b3c68169834a56eae6cf
Cathan Bertram <55544660+CathanBertram@users.noreply.github.com>
2024-04-14 12:56:43 +0100

    Dragging Points

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -6,29 +6,74 @@ public class InputHandler : MonoBehaviour
 {
     public MultiPointBezierCurve bezierCurve;
     public Camera mainCamera;
-    void Update()
+    private Vector2 mouseDragStart;
+    private bool dragging;
+    private GameObject dragTarget = null;
+
+    Plane plane = new Plane(Vector3.forward, Vector3.zero);
+
+    private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            HandleLeftMouseDown();
+            HandleLeftMouseDown();            
+        }      
+        else if(Input.GetMouseButton(0))
+        {
+            HandleDrag();
         }
-        else if (Input.GetMouseButtonDown(1))
+        else if(Input.GetMouseButtonUp(0))
+        {
+            HandleLeftMouseUp();
+        }
+        else if (Input.GetMouseButtonUp(1))
         {
             HandleRightMouse();          
         }
     }
-    void HandleLeftMouseDown()
+
+    private void HandleLeftMouseDown()
     {
-        Plane p = new Plane(Vector3.forward, Vector3.zero);
+        mouseDragStart = Input.mousePosition;
+
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if(p.Raycast(ray, out float distance))
+        if(Physics.Raycast(ray, out var hit))
+        {
+            if (hit.transform.gameObject.TryGetComponent<DebugPoint>(out var debugPoint))
+            {
+                dragTarget = hit.transform.gameObject;
+                dragging = true;
+            }
+        }
+    }
+    private void HandleDrag()
+    {
+        if (dragTarget == null) return;
+
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            var position = ray.GetPoint(distance);
+            dragTarget.transform.position = position;
+        }
+    }
+    private void HandleLeftMouseUp()
+    {
+        if (dragging)
+        {
+            dragging = false;
+            dragTarget = null;
+            return;
+        }
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if(plane.Raycast(ray, out float distance))
         {
             var position = ray.GetPoint(distance);
             bezierCurve.AddPoint(position);
         }
     }
 
-    void HandleRightMouse()
+    private void HandleRightMouse()
     {
         if (Physics.Raycast(mainCamera.ScreenPointToRay(Input.mousePosition), out var hit))
         {
